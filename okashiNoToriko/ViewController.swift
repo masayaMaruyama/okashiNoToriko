9ed7fdd5f304392f2454f3500303ec565faf6a90
masaya.maruyama <yayewanshan2@gmail.com>
2022-03-11 05:11:13 +0900

    Initial Commit

---
+++ okashiNoToriko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  okashiNoToriko
+//
+//  Created by jobs steve on 2022/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
